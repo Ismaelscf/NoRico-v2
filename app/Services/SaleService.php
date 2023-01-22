76f76cf0776b9f58e29a9a99078979133ba20718
Leandro Edson Silva de Araujo <leandro.araujo@stc.ma.gov.br>
2023-01-22 00:28:56 -0300

    Atualização de vendas visão admin

--- app/Services/SaleService.php
+++ app/Services/SaleService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Repositories\SaleRepository;
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
