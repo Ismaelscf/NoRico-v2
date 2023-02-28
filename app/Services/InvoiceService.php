6c486a0dde977d72f817b2031649fb58c17c932b
Leandro Edson Silva de Araujo <leandro.araujo@stc.ma.gov.br>
2023-02-28 00:33:55 -0300

    Retornando todos os dados da fatura para salvar no banco de dados

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -28,35 +28,48 @@ class InvoiceService
         $vendas = array();
         // dd($actors[1]->user->id, $actors[1]->user->payday);
         foreach ($actors as $actor) {
-            array_push($vendas, DB::table('users')
-            ->leftJoin('sales', function ($join) use ($actor) {
+
+            $data_vencimento = '2023-03-'.$actor->user->payday;
+
+            array_push($vendas, [DB::table('users')
+            ->leftJoin('sales', function ($join) use ($actor, $data_vencimento) {
                 $join->on('users.id', '=', 'sales.user_id')
-                    ->where('sale_date', '>=', '2023-03'.$actor->user->payday);
+                    ->where('sale_date', '<=', $data_vencimento);
             })
             ->select('users.id as user_id', DB::raw('COALESCE(SUM(total_sale), 0) AS total_sale'), DB::raw('COALESCE(SUM(discount), 0) AS discount'))
             ->where('users.id', '=', $actor->user->id)
             ->groupBy('users.id')
-            ->get());
+            ->get(), $data_vencimento]);
         
             // Use os resultados da query para criar as faturas para cada cliente do usuário atual.
         }
         // dd($vendas);
 
-        
-        if($vendas[36][0]->total_sale > 0){
-            $rico = $vendas[36][0]->discount * 0.8;
-            $benefico = $vendas[36][0]->discount * 0.2;
-        }
-        
+        foreach($vendas as $venda){
 
-        dd(
-            "id do usuário: ".$actors[36]->user->id,
-            "nome do usuário: ".$actors[36]->user->name,
-            "Total em compras: ".$vendas[36][0]->total_sale,
-            "Total de descontos recebidos: ".$vendas[36][0]->discount,
-            "Valor a pagar para o Rico: ".$rico,
-            "Valor de beneficio recebido: ".$benefico
-        );
+            if($venda[0][0]->total_sale > 0){
+                $rico = $venda[0][0]->discount * 0.8;
+                $benefico = $venda[0][0]->discount * 0.2;
+            } else {
+                $rico = 0;
+                $benefico = 0;
+            }
+
+            echo 
+                // "id do usuário: ".$actors[36]->user->id,
+                "ID do usuário das vendas: ".$venda[0][0]->user_id.
+                // "nome do usuário: ".$actors[36]->user->name,
+                "<br>Total em compras: ".$venda[0][0]->total_sale.
+                "<br>Total de descontos recebidos: ".$venda[0][0]->discount.
+                "<br>Valor a pagar para o Rico: ".$rico.
+                "<br>Valor de beneficio recebido: ".$benefico.
+                "<br>Data de Vencimento: ".$venda[1].
+                "<br>Status: ".
+                "<br><br><br>"
+            ;
+        }
+
+        
 
 
         return $actors;
