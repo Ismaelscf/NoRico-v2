75b337f0fad124fda6314b3db5c65874c9658ebe
Leandro Edson Silva de Araujo <leandro.araujo@stc.ma.gov.br>
2023-03-01 23:20:55 -0300

    Retornando todas as informações do banco para serem inseridas na nova tabela

--- app/Repositories/InvoiceRepository.php
+++ app/Repositories/InvoiceRepository.php
@@ -17,13 +17,17 @@ class InvoiceRepository
         $this->sale = $sale;
     }
 
-    public function generateInvoiceUser($user_id, $initial_date, $final_date){
+    public function generateInvoiceUser($initial_date, $final_date){
 
-        return $this->sale->select('user_id', DB::raw('SUM(total_sale) as total_sales'), DB::raw('SUM(discount) as total_discount'))
-        ->where('user_id', '=', $user_id)
+        return $this->sale->select('sales.user_id', 'users.payday', DB::raw('SUM(total_sale) as total_sales'), DB::raw('SUM(discount) as total_discount'))
+        ->join('users', 'users.id', '=', 'sales.user_id')
+        ->join('actors', 'actors.user_id', '=', 'users.id')
         ->whereBetween('sale_date', [$initial_date, $final_date])
-        ->groupBy('user_id')
-        ->get();;
+        ->where('total_sale', '>', 0)
+        ->where('actors.function', '=', 'cliente')
+        ->groupBy('sales.user_id', 'users.payday')
+        ->orderBy('sales.user_id')
+        ->get();
 
         // return $this->sale->where('user_id', '=', $user_id)->where('sale_date', '>=', $initial_date)->where('sale_date', '<=', $final_date)->sum('total_sale')->sum('discount')->get();
     }
--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -25,71 +25,25 @@ class InvoiceService
     }
 
     public function generateInvoiceBatch(){
-        $actors = $this->actorService->getAllClients();
 
-        $sales = array();
-        // dd($actors[1]->user->id, $actors[1]->user->payday);
-        foreach ($actors as $actor) {
+        $initial_date = date('Y-m-d', strtotime('first day of last month'));
+        $final_date = date('Y-m-d', strtotime('last day of last month'));
 
-            $user_id = $actor->user->id;
+        $invoice = $this->invoiceRepository->generateInvoiceUser($initial_date, $final_date);
 
-            $initial_date = date('Y-m-d', strtotime('first day of last month'));
-            $final_date = date('Y-m-d', strtotime('last day of last month'));
-
-            // $data_vencimento = '2023-03-'.$actor->user->payday;
-
-            $invoice = $this->invoiceRepository->generateInvoiceUser($user_id, $initial_date, $final_date);
-
-            if(!$invoice->isEmpty()){
-                array_push($sales, $invoice);
-            } else {
-                array_push($sales, [['user_id' => $user_id, 'total_sale' => 0, 'total_discount' => 0]]);
-            }
-
-            
-
-            // array_push($vendas, [DB::table('users')
-            // ->leftJoin('sales', function ($join) use ($actor, $data_vencimento) {
-            //     $join->on('users.id', '=', 'sales.user_id')
-            //         ->where('sale_date', '<=', $data_vencimento);
-            // })
-            // ->select('users.id as user_id', DB::raw('COALESCE(SUM(total_sale), 0) AS total_sale'), DB::raw('COALESCE(SUM(discount), 0) AS discount'))
-            // ->where('users.id', '=', $actor->user->id)
-            // ->groupBy('users.id')
-            // ->get(), $data_vencimento]);
-        
-            // Use os resultados da query para criar as faturas para cada cliente do usuário atual.
-        }
-        dd($sales);
-
-        foreach($sales as $venda){
-
-            if($venda[0]->total_sale > 0){
-                $rico = $venda[0]->total_discount * 0.8;
-                $benefico = $venda[0]->total_discount * 0.2;
-            } else {
-                $rico = 0;
-                $benefico = 0;
-            }
+        foreach($invoice as $venda){
 
             echo 
-                // "id do usuário: ".$actors[36]->user->id,
-                "ID do usuário das vendas: ".$venda[0]->user_id.
-                // "nome do usuário: ".$actors[36]->user->name,
-                "<br>Total em compras: ".$venda[0]->total_sale.
-                "<br>Total de descontos recebidos: ".$venda[0]->discount.
-                "<br>Valor a pagar para o Rico: ".$rico.
-                "<br>Valor de beneficio recebido: ".$benefico.
-                "<br>Data de Vencimento: ".
-                "<br>Status: ".
-                "<br><br><br>"
+                "ID do usuário das vendas: ".$venda->user_id.
+                "<br>Total em compras: ".$venda->total_sales.
+                "<br>Total de descontos recebidos: ".$venda->total_discount.
+                "<br>Valor a pagar para o Rico: ".$venda->total_discount * 0.8.
+                "<br>Valor de beneficio recebido: ".$venda->total_discount * 0.2.
+                "<br>Data de Vencimento: ".date('Y')."-".date('m')."-".$venda->payday.
+                "<br>Status: fechada
+                <br><br><br>"
             ;
         }
-
-        
-
-
-        return $actors;
     }
 
     public function generateInvoice(){
